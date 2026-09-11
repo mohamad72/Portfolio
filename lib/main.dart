3966be7025ea097d3e7e3971920510d40861a62a
unknown <mohrock@gmail.com>
2026-09-11 19:05:02 +0330

    changed gradle vrsion

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'src/app/app.dart';
 import 'src/shared/di/di_config.dart';
 
 Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies();
   runApp(const PortfolioApp());
