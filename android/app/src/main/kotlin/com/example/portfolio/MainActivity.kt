03340de29c079debd0448d8778d05b84efd1ada6
unknown <mohrock@gmail.com>
2026-09-11 19:25:13 +0330

    added permission in manifest

--- android/app/src/main/kotlin/com/example/portfolio/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/portfolio/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.portfolio
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
